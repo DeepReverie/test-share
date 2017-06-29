08ef3cada8d6b48bc2e77e6a2e8606076a6fd7ae
zheng_c <zheng_c@worksap.co.jp>
2017-06-29 15:37:49 +0800

    Initial commit

---
+++ src/com/chiucheng/demo/HelloWorld.java
@@ -0,0 +1,10 @@
+package com.chiucheng.demo;
+
+/**
+ * Created by zheng_c on 2017/6/29.
+ */
+public class HelloWorld {
+    public static void main(String args[]){
+        System.out.println("Hello World");
+    }
+}
